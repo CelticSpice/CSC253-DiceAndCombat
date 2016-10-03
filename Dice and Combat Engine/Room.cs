23ee94d408089f8c9535642cbe5cd8f7cb91875e
Timothy Burns <celticspice@openmailbox.org>
2016-10-03 16:51:39 -0400

    Add array of 4 Rooms to Room

--- Dice and Combat Engine/Room.cs
+++ Dice and Combat Engine/Room.cs
@@ -1,6 +1,6 @@
 ﻿/*
     This class represents a room in the game
-    9/22/2016
+    10/3/2016
     CSC 253 0001 - Dice and Combat Engine
     Author: James Alves, Shane McCann, Timothy Burns
 */
@@ -12,18 +12,14 @@ namespace Dice_and_Combat_Engine
     class Room
     {
         // Fields
-        /*
-        private int _id;                    Do we need these?
-        private string _desc;
-        */
         private string _roomName;
         private List<Creature> _denizens;   // Creatures in the room
         private List<Item> _contents;       // Items in the room
-        private Room nextRoom, previousRoom;
+        private Room[] _exits;
 
         /*
             Constructor
-            Accepts the room name and the number of creatures and items in the room
+            Accepts the room name and the number of creatures and items initially in the room
         */
 
         public Room(string name, int numCreatures, int numItems)
@@ -31,60 +27,10 @@ namespace Dice_and_Combat_Engine
             _roomName = name;
             _denizens = new List<Creature>(numCreatures);
             _contents = new List<Item>(numItems);
-            nextRoom = null;
-            previousRoom = null;
+
+            const int NUM_EXITS = 4;
+            _exits = new Room[NUM_EXITS];
         }
-
-        /*
-            The SetNextRoom method sets the next room that this room leads to
-        */
-
-        public void SetNextRoom(Room next)
-        {
-            nextRoom = next;
-        }
-
-        /*
-            The GetNextRoom method returns the next room that this room leads to
-        */
-
-        public Room GetNextRoom()
-        {
-            return nextRoom;
-        }
-
-        /*
-            The SetPreviousRoom method sets the previous room that this room leads to
-        */
-
-        public void SetPreviousRoom(Room previous)
-        {
-            previousRoom = previous;
-        }
-
-        /*
-            The GetPreviousRoom method returns the previous room that this room leads to
-        */
-
-        public Room GetPreviousRoom()
-        {
-            return previousRoom;
-        }
-
-        /*
-            The ToString method returns a string representation of the object
-        */
-
-        public override string ToString()
-        {
-            string str = "Room: " + _roomName;
-                      // "\nDescription: " + _desc;
-            return str;
-        }
-
-        /***************************************
-        *  Should the properties have setters? *
-        ****************************************/
 
         /*
             RoomName property
@@ -111,6 +57,15 @@ namespace Dice_and_Combat_Engine
         public List<Item> Contents
         {
             get { return _contents; }
+        }
+
+        /*
+            Exits property
+        */
+
+        public Room[] Exits
+        {
+            get { return _exits; }
         }
     }
 }
