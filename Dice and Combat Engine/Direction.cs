02e595ca204f33ea33ab0fae4e3f50c8691729bf
Timothy Burns <celticspice@openmailbox.org>
2016-10-03 15:29:40 -0400

    Create TODO file | Create Direction enum

---
+++ Dice and Combat Engine/Direction.cs
@@ -0,0 +1,11 @@
+﻿/*
+    Direction enumeration
+    10/3/2016
+    CSC 253 0001 - Dice and Combat
+    Author: James Alves, Shane McCann, Timothy Burns
+*/
+
+namespace Dice_and_Combat_Engine
+{
+    enum Direction { North, South, East, West }
+}
