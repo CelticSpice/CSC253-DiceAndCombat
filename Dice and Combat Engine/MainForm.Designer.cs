ff1e4d00456c07b76418dca6c19287c618721268
Timothy Burns <celticspice@openmailbox.org>
2016-10-04 01:07:10 -0400

    Redesign UI

--- Dice and Combat Engine/MainForm.Designer.cs
+++ Dice and Combat Engine/MainForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.attackButton = new System.Windows.Forms.Button();
             this.playerNameTextBox = new System.Windows.Forms.TextBox();
             this.playerHPTextBox = new System.Windows.Forms.TextBox();
             this.playerHPLabel = new System.Windows.Forms.Label();
@@ -47,30 +46,13 @@
             this.feedbackList = new System.Windows.Forms.ListBox();
             this.playerImgBox = new System.Windows.Forms.PictureBox();
             this.creatureImgBox = new System.Windows.Forms.PictureBox();
-            this.playerInventoryBox = new System.Windows.Forms.GroupBox();
-            this.playerInventoryList = new System.Windows.Forms.ListBox();
             this.roomNameLbl = new System.Windows.Forms.Label();
-            this.previousBtn = new System.Windows.Forms.Button();
-            this.nextBtn = new System.Windows.Forms.Button();
-            this.useItemBtn = new System.Windows.Forms.Button();
-            this.creatureListBox = new System.Windows.Forms.GroupBox();
-            this.creatureList = new System.Windows.Forms.ListBox();
+            this.commandTxtBox = new System.Windows.Forms.TextBox();
+            this.instructionLbl = new System.Windows.Forms.Label();
+            this.actionBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.playerImgBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.creatureImgBox)).BeginInit();
-            this.playerInventoryBox.SuspendLayout();
-            this.creatureListBox.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // attackButton
-            // 
-            this.attackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.attackButton.Location = new System.Drawing.Point(294, 382);
-            this.attackButton.Name = "attackButton";
-            this.attackButton.Size = new System.Drawing.Size(220, 33);
-            this.attackButton.TabIndex = 0;
-            this.attackButton.Text = "&Attack";
-            this.attackButton.UseVisualStyleBackColor = true;
-            this.attackButton.Click += new System.EventHandler(this.attackButton_Click);
             // 
             // playerNameTextBox
             // 
@@ -216,10 +198,10 @@
             // feedbackList
             // 
             this.feedbackList.FormattingEnabled = true;
-            this.feedbackList.Location = new System.Drawing.Point(239, 119);
+            this.feedbackList.Location = new System.Drawing.Point(162, 55);
             this.feedbackList.Name = "feedbackList";
             this.feedbackList.SelectionMode = System.Windows.Forms.SelectionMode.None;
-            this.feedbackList.Size = new System.Drawing.Size(331, 251);
+            this.feedbackList.Size = new System.Drawing.Size(486, 303);
             this.feedbackList.TabIndex = 19;
             // 
             // playerImgBox
@@ -238,24 +220,6 @@
             this.creatureImgBox.TabIndex = 21;
             this.creatureImgBox.TabStop = false;
             // 
-            // playerInventoryBox
-            // 
-            this.playerInventoryBox.Controls.Add(this.playerInventoryList);
-            this.playerInventoryBox.Location = new System.Drawing.Point(12, 209);
-            this.playerInventoryBox.Name = "playerInventoryBox";
-            this.playerInventoryBox.Size = new System.Drawing.Size(142, 217);
-            this.playerInventoryBox.TabIndex = 22;
-            this.playerInventoryBox.TabStop = false;
-            this.playerInventoryBox.Text = "Inventory";
-            // 
-            // playerInventoryList
-            // 
-            this.playerInventoryList.FormattingEnabled = true;
-            this.playerInventoryList.Location = new System.Drawing.Point(7, 20);
-            this.playerInventoryList.Name = "playerInventoryList";
-            this.playerInventoryList.Size = new System.Drawing.Size(129, 186);
-            this.playerInventoryList.TabIndex = 0;
-            // 
             // roomNameLbl
             // 
             this.roomNameLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -267,74 +231,44 @@
             this.roomNameLbl.Text = "Room Name";
             this.roomNameLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // previousBtn
+            // commandTxtBox
             // 
-            this.previousBtn.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.previousBtn.Enabled = false;
-            this.previousBtn.Image = global::Dice_and_Combat_Engine.Properties.Resources.leftArrow;
-            this.previousBtn.Location = new System.Drawing.Point(306, 55);
-            this.previousBtn.Name = "previousBtn";
-            this.previousBtn.Size = new System.Drawing.Size(66, 32);
-            this.previousBtn.TabIndex = 24;
-            this.previousBtn.UseVisualStyleBackColor = false;
-            this.previousBtn.Click += new System.EventHandler(this.previousBtn_Click);
+            this.commandTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandTxtBox.Location = new System.Drawing.Point(279, 400);
+            this.commandTxtBox.Name = "commandTxtBox";
+            this.commandTxtBox.Size = new System.Drawing.Size(331, 26);
+            this.commandTxtBox.TabIndex = 24;
             // 
-            // nextBtn
+            // instructionLbl
             // 
-            this.nextBtn.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.nextBtn.Enabled = false;
-            this.nextBtn.Image = global::Dice_and_Combat_Engine.Properties.Resources.rightArrow;
-            this.nextBtn.Location = new System.Drawing.Point(437, 55);
-            this.nextBtn.Name = "nextBtn";
-            this.nextBtn.Size = new System.Drawing.Size(66, 32);
-            this.nextBtn.TabIndex = 25;
-            this.nextBtn.UseVisualStyleBackColor = false;
-            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            this.instructionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLbl.Location = new System.Drawing.Point(342, 373);
+            this.instructionLbl.Name = "instructionLbl";
+            this.instructionLbl.Size = new System.Drawing.Size(206, 24);
+            this.instructionLbl.TabIndex = 25;
+            this.instructionLbl.Text = "Enter Command Below";
+            this.instructionLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // useItemBtn
+            // actionBtn
             // 
-            this.useItemBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.useItemBtn.Location = new System.Drawing.Point(160, 395);
-            this.useItemBtn.Name = "useItemBtn";
-            this.useItemBtn.Size = new System.Drawing.Size(56, 31);
-            this.useItemBtn.TabIndex = 26;
-            this.useItemBtn.Text = "&Use";
-            this.useItemBtn.UseVisualStyleBackColor = true;
-            this.useItemBtn.Click += new System.EventHandler(this.useItemBtn_Click);
-            // 
-            // creatureListBox
-            // 
-            this.creatureListBox.Controls.Add(this.creatureList);
-            this.creatureListBox.Location = new System.Drawing.Point(655, 209);
-            this.creatureListBox.Name = "creatureListBox";
-            this.creatureListBox.Size = new System.Drawing.Size(142, 217);
-            this.creatureListBox.TabIndex = 23;
-            this.creatureListBox.TabStop = false;
-            this.creatureListBox.Text = "Creatures in Room";
-            // 
-            // creatureList
-            // 
-            this.creatureList.FormattingEnabled = true;
-            this.creatureList.Location = new System.Drawing.Point(7, 20);
-            this.creatureList.Name = "creatureList";
-            this.creatureList.Size = new System.Drawing.Size(129, 186);
-            this.creatureList.TabIndex = 0;
-            this.creatureList.SelectedIndexChanged += new System.EventHandler(this.creatureList_SelectedIndexChanged);
+            this.actionBtn.Location = new System.Drawing.Point(198, 400);
+            this.actionBtn.Name = "actionBtn";
+            this.actionBtn.Size = new System.Drawing.Size(75, 26);
+            this.actionBtn.TabIndex = 26;
+            this.actionBtn.Text = "Do Action";
+            this.actionBtn.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AcceptButton = this.attackButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Dice_and_Combat_Engine.Properties.Resources.backgroundBlur;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(809, 438);
-            this.Controls.Add(this.creatureListBox);
-            this.Controls.Add(this.useItemBtn);
-            this.Controls.Add(this.nextBtn);
-            this.Controls.Add(this.previousBtn);
+            this.Controls.Add(this.actionBtn);
+            this.Controls.Add(this.instructionLbl);
+            this.Controls.Add(this.commandTxtBox);
             this.Controls.Add(this.roomNameLbl);
-            this.Controls.Add(this.playerInventoryBox);
             this.Controls.Add(this.creatureImgBox);
             this.Controls.Add(this.playerImgBox);
             this.Controls.Add(this.feedbackList);
@@ -353,21 +287,16 @@
             this.Controls.Add(this.playerHPLabel);
             this.Controls.Add(this.playerHPTextBox);
             this.Controls.Add(this.playerNameTextBox);
-            this.Controls.Add(this.attackButton);
             this.Name = "MainForm";
             this.Text = "Dice and Combat Engine - Fight";
             ((System.ComponentModel.ISupportInitialize)(this.playerImgBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.creatureImgBox)).EndInit();
-            this.playerInventoryBox.ResumeLayout(false);
-            this.creatureListBox.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button attackButton;
         private System.Windows.Forms.TextBox playerNameTextBox;
         private System.Windows.Forms.TextBox playerHPTextBox;
         private System.Windows.Forms.Label playerHPLabel;
@@ -386,14 +315,10 @@
         private System.Windows.Forms.ListBox feedbackList;
         private System.Windows.Forms.PictureBox playerImgBox;
         private System.Windows.Forms.PictureBox creatureImgBox;
-        private System.Windows.Forms.GroupBox playerInventoryBox;
-        private System.Windows.Forms.ListBox playerInventoryList;
         private System.Windows.Forms.Label roomNameLbl;
-        private System.Windows.Forms.Button previousBtn;
-        private System.Windows.Forms.Button nextBtn;
-        private System.Windows.Forms.Button useItemBtn;
-        private System.Windows.Forms.GroupBox creatureListBox;
-        private System.Windows.Forms.ListBox creatureList;
+        private System.Windows.Forms.TextBox commandTxtBox;
+        private System.Windows.Forms.Label instructionLbl;
+        private System.Windows.Forms.Button actionBtn;
     }
 }
 
