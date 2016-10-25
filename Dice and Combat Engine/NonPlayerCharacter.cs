a26537c78a2f82bf926494bdce3bf51d824ffdc7
Timothy Burns <celticspice@openmailbox.org>
2016-10-25 13:07:49 -0400

    Merge James Alves' changes

---
+++ Dice and Combat Engine/NonPlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_and_Combat_Engine
+{
+    class NonPlayerCharacter : Creature
+    {
+        private List<string> _responces;
+
+        public NonPlayerCharacter(List<string> responces, string desc) :
+            base(desc)
+        {
+            _responces = responces;
+        }
+    }
+}
